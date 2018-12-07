9be8caeae99b83158b811d74d6f794e7cdd2ec82
darpan pathak <pathakdarpan77@gmail.com>
2018-12-07 10:22:15 +0000

    initial commit from c9

--- README.md
+++
@@ -1,2 +0,0 @@
-# aws-ses-example
-Read email addresses from excel file and send email with amazon's SES service
---
+++ server.js
@@ -0,0 +1,60 @@
+const xlsx = require('xlsx');
+const AWS = require("aws-sdk");
+const fs = require("fs");
+
+AWS.config.update({
+    accessKeyId: "AKIAIQHNE7SBRWJCHANA",
+    secretAccessKey: "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",
+    region: "us-east-1"
+});
+
+const ses = new AWS.SES({
+    apiVersion: "2010-12-01"
+});
+
+function sendEmail() {
+
+     let workbook = xlsx.readFile('./inkloft.xlsx');
+    let data = xlsx.utils.sheet_to_json(workbook.Sheets["Sheet1"]);
+
+    let emailIds = data.map((item) => {
+        return item.email;
+    });
+
+
+    let htmlText = fs.readFileSync("./newsletter.txt").toString();
+
+    //console.log(htmlText);
+
+    const params = {
+        Destination: {
+            ToAddresses: emailIds // Email address/addresses that you want to send your email
+        },
+        Message: {
+            Body: {
+                Html: {
+                    // HTML Format of the email
+                    Charset: "UTF-8",
+                    Data: htmlText
+                }
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: "Test email"
+            }
+        },
+        Source: "info@inkloft.in"
+    };
+
+    const sendEmail = ses.sendEmail(params).promise();
+
+    sendEmail
+        .then(data => {
+            console.log("Done" + JSON.stringify(data));
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+sendEmail();
